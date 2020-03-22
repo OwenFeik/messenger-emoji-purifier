5eef4783231b37fb9d828527bc0dd60b7ab9f1ae
OwenFeik <owenhfeik@gmail.com>
2020-03-22 23:42:49 +1100

    more robust detection and replacement

--- replace-emojis.js
+++ replace-emojis.js
@@ -1,4 +1,3 @@
-console.log("start");
 
 function getMessages() {
 	setTimeout(function() {
@@ -13,19 +12,46 @@ function getMessages() {
 	}, 500);
 }
 
-function main(messages) {
-	console.log(messages);
-	console.log("done");
+function purify(child) {
+    console.log(child);
+    try {
+        if (child.getAttribute("class").includes("clearfix _o46 _3erg _3i_m _nd_")) {
+            var bbl = child.children[0];
+            var msg = bbl.children[1];
+            var content = msg.children[0];
+            if (content.tagName === 'DIV') {
+                var emoji = content.children[0];
+                if (emoji.tagName === 'IMG') {
+                    text = msg.getAttribute('aria-label');
+                    if (text) {
+                        child.setAttribute('class', 'clearfix _o46 _3erg _3i_m _nd_ _8lma direction_ltr text_align_ltr');
+                        bbl.setAttribute('class', '_3058 _ui9 _hh7 _6ybn _s1- _52mr _43by _6ybp _3oh-');
+                        msg.innerHTML = '<span class="_30h- _58nk">'.concat(msg.getAttribute('aria-label')).concat('</span>');
+                    }
+                }
+            }
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
 
-	const config = {childList: true, subtree: true };
+function main(messages) {
+    console.log(messages);
+
+    // Replace all emojis found initially
+    var arr = Array.from(document.getElementsByClassName('clearfix _o46 _3erg _3i_m _nd_'));
+    arr.forEach(purify);
+
+	const config = {childList: true, subtree: true, attributes: true };
 
 	const callback = function(mutationsList, observer) {
 		for (let mutation of mutationsList) {
 			if (mutation.type === 'childList') {
-				e = mutation.target
-				if (e.getAttribute("class") === "_aok _7i2m") {
-					console.log(e);
-					e.children[0].innerHTML = '<div class="_3058 _ui9 _hh7 _6ybn _s1- _52mr _43by _6ybp _3oh-">'.concat(e.getAttribute("aria-label")).concat("</div>");	
+                e = mutation.target
+				if (e.getAttribute("class") === "_41ud") {
+                    arr = Array.from(e.children);
+                    arr.forEach(purify);
 				}
 			}
 		}
