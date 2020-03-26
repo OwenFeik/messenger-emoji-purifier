4b94d63f2fa9efcd524ebdaaadad0926cb8f6c8d
OwenFeik <owenhfeik@gmail.com>
2020-03-26 12:30:59 +1100

    better style detection

--- purify-emojis.js
+++ purify-emojis.js
@@ -28,69 +28,6 @@ function checkPageChange(observer) {
     }, 500, observer);
 }
 
-function getStyle(child) {
-    function get(c) {
-        try {
-            return c.children[0].getAttribute('style');
-        }
-        catch {
-            return '';
-        }
-    }
-
-    sib = child.parentNode.children[1];
-    do {
-        var bbl_style = get(sib);
-        if (bbl_style) {
-            return bbl_style;
-        }
-    } while (sib = sib.nextSibling);
-
-    return '';
-}
-
-function getElements(child) {
-    var bbl = child.firstChild;
-    do {
-        if (bbl.getAttribute('class').includes('_3058 _ui9 _hh7 _6ybn _s1- _52mr')) {
-            break;
-        }
-    } while (bbl = bbl.nextSibling);
-
-    var msg = bbl.firstChild;
-    do {
-        if (msg.getAttribute('class').includes('_aok _7i2m')) {
-            break;
-        }
-    } while (msg = msg.nextSibling);
-
-    console.log(bbl);
-    console.log(msg);
-    return [bbl, msg];
-}
-
-function purify(child) {
-    try {
-        if (child.getAttribute('class').includes('clearfix _o46 _3erg')) {
-            var bbl, msg;
-            [bbl, msg] = getElements(child);
-            if (child.innerHTML.includes('<img') && child.innerHTML.includes('src="https://static.xx.fbcdn.net/images/emoji.php/')) {
-                text = msg.getAttribute('aria-label');
-                if (text) {
-                    bbl.setAttribute('style', getStyle(child));
-                    if (bbl.getAttribute('class').includes('_43by _6ybp')) {
-                        bbl.setAttribute('class', '_3058 _ui9 _hh7 _6ybn _s1- _52mr _43by _6ybp _3oh-');
-                    }
-                    else {
-                        bbl.setAttribute('class', '_3058 _ui9 _hh7 _6ybn _s1- _52mr _3oh-');
-                    }
-                    msg.innerHTML = '<span class="_30h- _58nk">'.concat(msg.getAttribute('aria-label')).concat('</span>');
-                }
-            }
-        }
-    } catch (error) { }
-}
-
 function startObserver(messages) {
     // Replace all emojis found initially
     var arr = Array.from(document.getElementsByClassName('clearfix _o46 _3erg _3i_m _nd_'));
@@ -116,3 +53,96 @@ function startObserver(messages) {
 }
 
 getMessages();
+
+function getStyle(child) {
+    // Extracts style from a "clearfix _3058 _ui9"
+    function getMessageStyle(m) {
+        try {
+            return m.children[0].getAttribute('style');
+        }
+        catch {
+            return '';
+        }
+    }
+
+    // Searches a "_41ud" for style
+    function getBlockStyle(b) {
+        var sender = child.getAttribute('class').includes('_nd_'); 
+        try {
+            var bbl_style = '';
+            
+            msg = b.children[1];
+            // sender's messages have different styling to other messages.
+            if (msg.getAttribute('class').includes('_nd_') != sender) {
+                return '';
+            }
+
+            do {
+                bbl_style = getMessageStyle(msg);
+                if (bbl_style) {
+                    return bbl_style;
+                }
+            } while (msg = msg.nextSibling);
+        }
+        catch { }
+        return '';
+    }
+
+    // iterate across _41ud elements starting from the childs parent
+    var blocks = Array.from(document.getElementsByClassName('_41ud'));
+    var block = child.parentNode;
+    blocks.splice(blocks.indexOf(block), 1);
+
+    do {
+        var bbl_style = getBlockStyle(block);
+        if (bbl_style) {
+            return bbl_style;
+        }
+    } while (block = blocks.pop())
+
+    return '';
+}
+
+function getElements(child) {
+    var bbl = child.firstChild;
+    do {
+        if (bbl.getAttribute('class').includes('_3058 _ui9 _hh7 _6ybn _s1- _52mr')) {
+            break;
+        }
+    } while (bbl = bbl.nextSibling);
+
+    var msg = bbl.firstChild;
+    do {
+        if (msg.getAttribute('class').includes('_aok _7i2m')) {
+            break;
+        }
+    } while (msg = msg.nextSibling);
+
+    return [bbl, msg];
+}
+
+function purify(child) {
+    try {
+        // this is an image
+        if (child.getAttribute('class').includes('_ylc')) { return; }
+
+        if (child.getAttribute('class').includes('clearfix _o46 _3erg')) {
+            var bbl, msg;
+            [bbl, msg] = getElements(child);
+            if (child.innerHTML.includes('<img') && child.innerHTML.includes('src="https://static.xx.fbcdn.net/images/emoji.php/')) {
+                text = msg.getAttribute('aria-label');
+                if (text) {
+                    bbl.setAttribute('style', getStyle(child));
+                    if (bbl.getAttribute('class').includes('_43by _6ybp')) {
+                        bbl.setAttribute('class', '_3058 _ui9 _hh7 _6ybn _s1- _52mr _43by _6ybp _3oh-');
+                    }
+                    else {
+                        bbl.setAttribute('class', '_3058 _ui9 _hh7 _6ybn _s1- _52mr _3oh-');
+                    }
+                    msg.innerHTML = '<span class="_30h- _58nk">'.concat(msg.getAttribute('aria-label')).concat('</span>');
+                }
+                console.log(child);
+            }
+        }
+    } catch (error) { console.log(error); }
+}
